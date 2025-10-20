d3d5b6a9a3f65d412d4b93555e4b047388222f5e
Alessandro <alessandrodionizzio@gmail.com>
2025-10-20 16:37:36 -0300

    controller_aluno

--- src/conexion/queries.py
+++ src/conexion/queries.py
@@ -2,6 +2,5 @@ import json
 import cx_Oracle
 from pandas import DataFrame
 
-import cx_Oracle
 
 print("Versão cx_Oracle:", cx_Oracle.version)
--- src/controller/controller_treino.py
+++ src/controller/controller_treino.py
@@ -1,31 +1,42 @@
-from connection import get_connection
-from models.treino import Treino
+from conexion import get_connection
+from model.treino import Treino
 
 class TreinoController:
     @staticmethod
     def listar_todos():
-        conn = get_connection()
         treinos = []
-        if conn:
-            cur = conn.cursor()
-            cur.execute("SELECT * FROM LABDATABASE.TREINO")
-            for row in cur:
-                treinos.append(Treino(*row))
-            cur.close()
-            conn.close()
+        try:
+            conn = get_connection()
+            with conn.cursor() as cur:
+                cur.execute("""
+                    SELECT ID_TREINO, ID_ALUNO, ID_INSTRUTOR, TIPO, HORARIO, DIAS_SEMANA, FREQUENCIA
+                    FROM TREINO
+                """)
+                for row in cur:
+                    treinos.append(Treino(*row))
+        except Exception as e:
+            print(f"Erro ao listar treinos: {e}")
+        finally:
+            if conn:
+                conn.close()
         return treinos
 
     @staticmethod
     def inserir(id_aluno, id_instrutor, tipo, horario, dias_semana, frequencia):
-        conn = get_connection()
-        if conn:
-            cur = conn.cursor()
-            cur.execute("""
-                INSERT INTO LABDATABASE.TREINO 
-                (ID_TREINO, ID_ALUNO, ID_INSTRUTOR, TIPO, HORARIO, DIAS_SEMANA, FREQUENCIA)
-                VALUES (LABDATABASE.TREINO_ID_SEQ.NEXTVAL, :1, :2, :3, :4, :5, :6)
-            """, (id_aluno, id_instrutor, tipo, horario, dias_semana, frequencia))
-            conn.commit()
-            cur.close()
-            conn.close()
+        try:
+            conn = get_connection()
+            with conn.cursor() as cur:
+                cur.execute("""
+                    INSERT INTO TREINO 
+                    (ID_TREINO, ID_ALUNO, ID_INSTRUTOR, TIPO, HORARIO, DIAS_SEMANA, FREQUENCIA)
+                    VALUES (TREINO_ID_SEQ.NEXTVAL, :1, :2, :3, :4, :5, :6)
+                """, (id_aluno, id_instrutor, tipo, horario, dias_semana, frequencia))
+                conn.commit()
             print("Treino inserido com sucesso!")
+            return True
+        except Exception as e:
+            print(f"Erro ao inserir treino: {e}")
+            return False
+        finally:
+            if conn:
+                conn.close()
